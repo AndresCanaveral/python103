1a154eb0cc30f54ab594824a8c294d4ec6814e3f
AndresCanaveral <66961752+AndresCanaveral@users.noreply.github.com>
2023-06-28 21:47:11 +0000

    Prueba

---
+++ main.py
@@ -0,0 +1,22 @@
+set_contries = {'col', 'mex','bol'}
+print(set_contries)
+print(type(set_contries))
+
+set_numbers = {1, 2, 2, 3, 433, 23}
+print(set_numbers)
+
+set_types = {1, 'hola', False, 12.12}
+print(set_types)
+
+set_from_string = set('hoola')
+print(set_from_string)
+
+set_from_tuple = set(('abc','cbv','as','abc'))
+print(set_from_tuple)
+
+numbers = [1,2,3,1,2,3,4]
+set_numbers = set(numbers)
+print(set_numbers)
+
+unique_numbers = list(set_numbers)
+print(unique_numbers)
