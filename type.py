de54eba55d7696cf509f1c4a3dfc95aeecf1b9cc
AndresCanaveral <66961752+AndresCanaveral@users.noreply.github.com>
2023-07-05 23:47:34 +0000

    Types

---
+++ type.py
@@ -0,0 +1,25 @@
+#Tipo de datos
+print("*" * 40)
+print("Entero")
+numero = 6
+print(numero, type(numero))
+
+print("*" * 40)
+print("Flotante")
+flotante = 6.6
+print(flotante, type(flotante))
+
+print("*" * 40)
+print("Numero complejo")
+num_complejo = 5 + 6j
+print(num_complejo, type(num_complejo))
+
+print("*" * 40)
+print("String")
+nombre = "Andres"
+print(nombre, type(nombre))
+
+print("*" * 40)
+print("Boolean")
+boolean = 3 == 3
+print(boolean, type(boolean))
