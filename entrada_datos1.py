561b86ad9f23c73a29bd51a0261bda67bbc3c331
AndresCanaveral <66961752+AndresCanaveral@users.noreply.github.com>
2023-07-06 00:18:46 +0000

    Inputs

---
+++ entrada_datos1.py
@@ -0,0 +1,22 @@
+# Entrada de datos.
+# Ejemplo 1
+'''
+palabra = input("Ingresa una palabra: ")
+entero = int(input("Ingresa un numero entero: "))
+flotante = float(input("Ingresa un numero flotante: "))
+complejo = complex(input("Ingresa un numero complejo: "))
+
+print("String: ", palabra)
+print("Entero: ", entero)
+print("Flotante: ", flotante)
+print("Complejo: ", complejo)
+'''
+# Ejemplo 2)
+
+nombre = input("Ingrese su nombre: ")
+print("Hola " + nombre + " Vamos a realizar una suma.")
+
+n1 = int(input("Coloca por favor el numero 1: "))
+n2 = int(input("Coloca por favor el numero 2: "))
+resultado = n1 + n2
+print(nombre + ", El resultado es: ", resultado)
