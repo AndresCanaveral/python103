83d65e029f320fe6e1896229465f9e1657224cc3
AndresCanaveral <66961752+AndresCanaveral@users.noreply.github.com>
2023-07-11 02:45:54 +0000

    multiple condicions

---
+++ condicionales_2.py
@@ -0,0 +1,20 @@
+# Condicionales multiples
+print("*" * 40)
+print("Convertidor de numeros a letras. ")
+print("*" * 40)
+
+num_uno = int(input("Cual es el numero que quieres convertir?: "))
+
+if num_uno == 1:
+  print("El numero es 'uno'. ")
+elif num_uno == 2:
+  print("El numero es 'dos'. ")
+elif num_uno == 3:
+  print("El numero es 'tres'. ")
+elif num_uno == 4:
+  print("El numero es 'cuatro'. ")
+elif num_uno == 5:
+  print("El numero es 'cinco'. ")
+else:
+  print("Este programa solo puede convertir hasta el numero 5.")
+
