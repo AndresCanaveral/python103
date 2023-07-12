3d5bdce60193a769ff47c554be6e291b5d52b12c
AndresCanaveral <66961752+AndresCanaveral@users.noreply.github.com>
2023-07-12 23:56:41 +0000

    update name

--- sistema_control_vacaional.py
+++ sistema_control_vacaional.py
@@ -10,33 +10,33 @@ anio = int(input("Escriba años: "))
 
 if clave == 1:
   if anio == 1:
-    print("6 dias de vacaiones. ")
+    print("el empleado", name, "6 dias de vacaiones. ")
   elif anio >= 2 and anio <= 6:
-    print("14 dias de vacaciones. ")
+    print("el empleado", name,"14 dias de vacaciones. ")
   elif anio >= 7:
-    print("20 dias de vacaciones. ")
+    print("el empleado", name,"20 dias de vacaciones. ")
   else:
-    print("Sin derecho a vacaciones. ")
+    print("el empleado", name,"Sin derecho a vacaciones. ")
     
 elif clave == 2:
   if anio == 1:
-    print("7 dias de vacaiones. ")
+    print("el empleado", name,"7 dias de vacaiones. ")
   elif anio >= 2 and anio <= 6:
-    print("15 dias de vacaciones. ")
+    print("el empleado", name,"15 dias de vacaciones. ")
   elif anio >= 7:
-    print("22 dias de vacaciones. ")
+    print("el empleado", name,"22 dias de vacaciones. ")
   else:
-    print("Sin derecho a vacaciones. ")
+    print("el empleado", name,"Sin derecho a vacaciones. ")
     
 elif clave == 3:
   if anio == 1:
-    print("10 dias de vacaiones. ")
+    print("el empleado", name,"10 dias de vacaiones. ")
   elif anio >= 2 and anio <= 6:
-    print("20 dias de vacaciones. ")
+    print("el empleado", name,"20 dias de vacaciones. ")
   elif anio >= 7:
-    print("30 dias de vacaciones. ")
+    print("el empleado", name,"30 dias de vacaciones. ")
   else:
-    print("Sin derecho a vacaciones. ")
+    print("el empleado", name,"Sin derecho a vacaciones. ")
 
 else:
   print("la clave no existe. ")
