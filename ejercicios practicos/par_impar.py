395945fcc40785929b3a3acc384437f3ff9b93d3
AndresCanaveral <66961752+AndresCanaveral@users.noreply.github.com>
2023-07-13 00:15:23 +0000

    ejercicio practico 2

---
+++ ejercicios practicos/par_impar.py
@@ -0,0 +1,8 @@
+print("Numero par o impar")
+
+number = int(input("Coloque el numero: \n"))
+
+if number % 2 == 0:
+  print("Numero ", number, "es par")
+else:
+  print("Numero ",number, "no es par")
