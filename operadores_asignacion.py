1df9419ec6b1ddc16f357940f05d2fe2e3bbde1b
AndresCanaveral <66961752+AndresCanaveral@users.noreply.github.com>
2023-07-13 22:54:58 +0000

    operadores de asignacion

---
+++ operadores_asignacion.py
@@ -0,0 +1,26 @@
+# Operadores de Asignacion
+
+nombre = "Hola "
+nombre += input("Escribe tu nombre: ")
+
+print(nombre, ", esto es el incremento y decremento de una variable \n")
+
+print("Incremento o aumento")
+x = 1
+print("Valor inicial de x es: ", x)
+
+x += 1
+x += 1
+x += 1
+x += 1
+print("El valor final de x es: ", x, "\n")
+
+print("Decremento o disminucion")
+print("Valor inicial de x es: ", x)
+
+x-=1
+x-=1
+x-=1
+x-=1
+
+print("El valor final de x es: ", x, "\n")
