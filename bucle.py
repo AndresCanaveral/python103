660872f64245933fccd1a9646c4db292bc9996f4
AndresCanaveral <66961752+AndresCanaveral@users.noreply.github.com>
2023-07-19 22:27:57 +0000

    Sentencia break

--- bucle.py
+++ bucle.py
@@ -1,5 +1,6 @@
 ## Bucle o ciclo while
 # Sucesion Fibonacci
+'''
 
 num_uno, num_dos = 0, 1
 
@@ -8,3 +9,18 @@ while num_dos <= 1597:
   num_uno = num_uno + num_dos
   num_dos = num_uno + num_dos
   
+'''
+
+# Sentencia Break
+
+print("While con la sentencia break \n")
+
+contador = 0
+
+while contador < 10:
+  contador += 1
+
+  if contador == 5:
+    break
+  print("valor actual de la variable ", contador)
+
